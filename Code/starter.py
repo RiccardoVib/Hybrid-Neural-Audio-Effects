9b0021bfbb49ecfde16b3e6c3de930eb2ab3aefc
RiccardoVib <riccardo.simionato.vib@gmail.com>
2024-09-23 12:12:16 +0200

    Update starter.py

--- Code/starter.py
+++ Code/starter.py
@@ -1,14 +1,22 @@
 from Training import train
 
-data_dir = '../../Files/Delay'
-epochs = [1, 10]
-units = 8
-lr = 3e-4
-b_size = 600
+
+"""
+main script
+
+"""
+# data_dir: the directory in which datasets are stored
+data_dir = '../../Files/'
+
+epochs = 200
+units = 8 # number of model's units
+b_size = 600 # batch size
+lr = 3e-4 # initial learning rate
+
+
 model = 'ED'
 
-
-dataset = 'CL1BTapePreamp'
+dataset = 'CL1BTapePreamp' # dataset to use
 cond = 1
 train(data_dir=data_dir,
       save_folder=model+dataset+'TCP',
